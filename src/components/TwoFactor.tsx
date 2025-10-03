65b1fc2d320ff04c838b7e14625447359335e301
Robojosef <robojosef@gmail.com>
2025-10-03 10:14:32 -0700

    Add secret location

--- src/components/TwoFactor.tsx
+++ src/components/TwoFactor.tsx
@@ -1,5 +1,6 @@
-import { Shield, Car } from "lucide-react";
+import { Shield } from "lucide-react";
 import chestKey from "../assets/chest-key-256.png";
+import secretLocation from "../assets/secret-location-256.png";
 
 export const TwoFactor = () => {
   return (
@@ -18,21 +19,21 @@ export const TwoFactor = () => {
         <div className="grid md:grid-cols-2 gap-8 max-w-5xl mx-auto">
           <div className="text-left p-8 bg-card rounded-xl shadow-soft border border-border">
             <div className="w-16 h-16 bg-red-100 dark:bg-red-950 rounded-full flex items-center justify-center mb-6">
-              <img src={chestKey} alt="Chest key" className="w-16 h-16" />
+              <img src={chestKey} alt="Chest key" className="w-8 h-8" />
             </div>
             <h3 className="text-xl font-semibold text-foreground mb-4">Factor 1: the recipient address</h3>
             <p className="text-muted-foreground">
-              This makes the recipient wallet the key to the treasure chest. Only the recipient wallet may collect the tokens.
+              The key to the treasure chest. Only the recipient wallet may collect the tokens.
             </p>
           </div>
           
           <div className="text-left p-8 bg-card rounded-xl shadow-soft border border-border">
             <div className="w-16 h-16 bg-blue-100 dark:bg-blue-950 rounded-full flex items-center justify-center mb-6">
-              <Car className="w-8 h-8 text-blue-600 dark:text-blue-400" />
+              <img src={secretLocation} alt="Secret location map" className="w-8 h-8" />
             </div>
             <h3 className="text-xl font-semibold text-foreground mb-4">Factor 2: the secret location</h3>
             <p className="text-muted-foreground">
-              This is a unique URL generated when you make your deposit. The recipient may not collect the tokens until you share the location with them. You can think of it as the map to the treasure. You may have the key, but you also need to know where the treasure chest is buried!
+              The map to the treasure chest. This is a unique URL generated when the tokens are deposited. The recipient may not collect the tokens until you share the location with them.
             </p>
           </div>
         </div>
